2f99680504fb96a9c00074ae92d7cf8956a48ab8
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-09-21 01:45:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HashRouter } from "react-router-dom";
+console.log("React app starting..."); 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <HashRouter basename="/template-design"> 
